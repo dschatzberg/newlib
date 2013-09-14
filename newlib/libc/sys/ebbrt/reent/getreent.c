5e2d2a09b88453501fa0833fb9d2535ca2df034f
Dan Schatzberg <schatzberg.dan@gmail.com>
2013-09-14 13:29:15 -0400

    Remove unnecessary reentrancy

--- newlib/libc/sys/ebbrt/reent/getreent.c
+++
@@ -1,9 +0,0 @@
-#include <reent.h>
-
-extern struct _reent *ebbrt_newlib_getreent(void);
-
-struct _reent *
-__getreent(void)
-{
-  return ebbrt_newlib_getreent();
-}
--- newlib/libc/sys/ebbrt/reent/initreent.c
+++
@@ -1,6 +0,0 @@
-#include <reent.h>
-
-void ebbrt_init_reent(struct _reent *reent)
-{
-  _REENT_INIT_PTR(reent);
-}
